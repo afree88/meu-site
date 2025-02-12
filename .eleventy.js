cec7c2775ac7b34c9104641d3f91d7284478beb1
talison <talisonlimasousa@gmail.com>
2025-02-12 11:37:11 -0300

    Primeiro commit do meu projeto

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,18 @@
-// imports for the various eleventy plugins (navigation & image)
+// importações para os vários plugins do Eleventy (navegação e imagem)
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const { DateTime } = require('luxon');
 const Image = require('@11ty/eleventy-img');
 const path = require('path');
 
-// allows the use of {% image... %} to create responsive, optimised images
-// CHANGE DEFAULT MEDIA QUERIES AND WIDTHS
+// permite o uso de {% image... %} para criar imagens responsivas e otimizadas
+// ALTERE AS QUERIES DE MÍDIA E LARGURAS PADRÃO
 async function imageShortcode(src, alt, className, loading, sizes = '(max-width: 600px) 400px, 850px') {
-  // don't pass an alt? chuck it out. passing an empty string is okay though
+  // se não passar um alt? ignore. passar uma string vazia é ok
   if (alt === undefined) {
-    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+    throw new Error(`Faltando \`alt\` na imagem responsiva de: ${src}`);
   }
 
-  // create the metadata for an optimised image
+  // cria os metadados para uma imagem otimizada
   let metadata = await Image(`${src}`, {
     widths: [200, 400, 850, 1920, 2500],
     formats: ['webp', 'jpeg'],
@@ -25,11 +25,11 @@ async function imageShortcode(src, alt, className, loading, sizes = '(max-width:
     },
   });
 
-  // get the smallest and biggest image for picture/image attributes
+  // obtém a menor e a maior imagem para os atributos picture/image
   let lowsrc = metadata.jpeg[0];
   let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
 
-  // when {% image ... %} is used, this is what's returned
+  // quando {% image ... %} é usado, isso é o que será retornado
   return `<picture class="${className}">
     ${Object.values(metadata)
       .map((imageFormat) => {
@@ -49,27 +49,27 @@ async function imageShortcode(src, alt, className, loading, sizes = '(max-width:
 }
 
 module.exports = function (eleventyConfig) {
-  // adds the navigation plugin for easy navs
+  // adiciona o plugin de navegação para navegações fáceis
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // allows css, assets, robots.txt and CMS config files to be passed into /public
+  // permite que arquivos CSS, assets, robots.txt e arquivos de configuração do CMS sejam copiados para /public
   eleventyConfig.addPassthroughCopy('./src/css/**/*.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
   eleventyConfig.addPassthroughCopy('./src/_redirects');
   eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
 
-  // open on npm start and watch CSS files for changes - doesn't trigger 11ty rebuild
+  // abre ao rodar o npm start e observa alterações nos arquivos CSS - não aciona a reconstrução do 11ty
   eleventyConfig.setBrowserSyncConfig({
     open: true,
     files: './public/css/**/*.css',
   });
 
-  // allows the {% image %} shortcode to be used for optimised iamges (in webp if possible)
+  // permite o uso do shortcode {% image %} para imagens otimizadas (em webp se possível)
   eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
 
-  // normally, 11ty will render dates on blog posts in full JSDate format (Fri Dec 02 18:00:00 GMT-0600). That's ugly
-  // this filter allows dates to be converted into a normal, locale format. view the docs to learn more (https://moment.github.io/luxon/api-docs/index.html#datetime)
+  // normalmente, o 11ty renderiza as datas nos posts do blog no formato completo de JSDate (Fri Dec 02 18:00:00 GMT-0600). Isso é feio
+  // esse filtro permite que as datas sejam convertidas para um formato normal e localizado. veja a documentação para aprender mais (https://moment.github.io/luxon/api-docs/index.html#datetime)
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
@@ -81,7 +81,7 @@ module.exports = function (eleventyConfig) {
       layouts: "_layouts",
       output: 'public',
     },
-    // allows .html files to contain nunjucks templating language
+    // permite que arquivos .html contenham a linguagem de template Nunjucks
     htmlTemplateEngine: 'njk',
   };
 };
